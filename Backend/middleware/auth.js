33abdef5f80c8b9e74ecbce1bd33ed474126510b
Jayashan Manodya <kpjmp28@gmail.com>
2026-04-07 17:31:20 +0530

    delete

--- Backend/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
-
-export { ClerkExpressRequireAuth };
-
-export const getUserId = (req) => {
-    return req.auth.userId;
-};
-
-export const requireAuth = ClerkExpressRequireAuth();
